84dba1182fa1a842d5c6b3db2ae09cb89342c70e
Athena <ajhung2@illinois.edu>
2022-11-16 14:17:50 -0500

    include recipe_page_num in db, remove old unused beautiful soup functions

--- acooknbook/ingredientlist.py
+++ acooknbook/ingredientlist.py
@@ -21,19 +21,14 @@ def get_eatyourbooks_pages(initial_url):
 
 
 def recipe_and_ingredients_db_from_pages(): 
-	# myurls = ["https://www.eatyourbooks.com/library/186630/ottolenghi-simple-a-cookbook", 
-	# 		"https://www.eatyourbooks.com/library/186630/ottolenghi-simple-a-cookbook/2", 
-	# 		"https://www.eatyourbooks.com/library/186630/ottolenghi-simple-a-cookbook/3", 
-	# 		"https://www.eatyourbooks.com/library/186630/ottolenghi-simple-a-cookbook/4", 
-	# 		"https://www.eatyourbooks.com/library/186630/ottolenghi-simple-a-cookbook/5", 
-	# 		"https://www.eatyourbooks.com/library/186630/ottolenghi-simple-a-cookbook/6"]
+	#myurls = ["https://www.eatyourbooks.com/library/186630/ottolenghi-simple-a-cookbook"]
 	myurls = get_eatyourbooks_pages("https://www.eatyourbooks.com/library/186630/ottolenghi-simple-a-cookbook")
 	#print(myurls)
 
 	with open("simpledb.csv", 'w') as c:
 		csvwriter = csv.writer(c)
 
-		c.write("Recipe, Ingredients\n")
+		c.write("Recipe,Page,Ingredients\n")
 
 
 		for myurl in myurls: 
@@ -47,6 +42,10 @@ def recipe_and_ingredients_db_from_pages():
 				#recipe title is the link text under class = "RecipeTitleExp"
 				recipe_title = b.find('a', href=True, class_="RecipeTitleExp").get_text()
 				print(recipe_title)
+
+				#
+				recipe_page_num = b.find('i', class_="PageNoExp").get_text().strip("(page ").strip(')')
+				print(" Page",recipe_page_num)
 				
 				#ingredients is under an li, where <b>Ingredients</b>. But don't want that part to be part of the ingredients in csv
 				lis = b.find_all('li')
@@ -59,81 +58,7 @@ def recipe_and_ingredients_db_from_pages():
 							break
 
 				#write in csv
-				csvwriter.writerow([recipe_title,recipe_ingredients])
-
-
-
-#don't need to use after inital txt doc created; actually don't need anymore but maybe should keep as beautiful soup reference? 
-def find_recipe_urls(): 
-	myurls = ["https://www.eatyourbooks.com/library/186630/ottolenghi-simple-a-cookbook", 
-			"https://www.eatyourbooks.com/library/186630/ottolenghi-simple-a-cookbook/2", 
-			"https://www.eatyourbooks.com/library/186630/ottolenghi-simple-a-cookbook/3", 
-			"https://www.eatyourbooks.com/library/186630/ottolenghi-simple-a-cookbook/4", 
-			"https://www.eatyourbooks.com/library/186630/ottolenghi-simple-a-cookbook/5", 
-			"https://www.eatyourbooks.com/library/186630/ottolenghi-simple-a-cookbook/6"]
-
-	with open("recipeurls.txt", 'w') as f:
-		for myurl in myurls: 
-			print(myurl)
-			page_html = requests.get(myurl).text 
-			page_soup = soup(page_html, "html.parser")
-
-			base_url = "https://www.eatyourbooks.com"
-			for ahref in page_soup.find_all('a', href=True, class_="RecipeTitleExp"): 
-				url_str = base_url+ahref['href']
-				print ('\t'+url_str)
-				f.write(url_str+'\n')
-
-
-#don't need to use after inital db.csv created; actually don't need anymore but maybe should keep as beautiful soup reference? 
-def create_new_ingredientsdbcsv(url_txt): 
-	#myurl = 'https://www.eatyourbooks.com/library/recipes/2023109/harissa-and-manchego-omelettes'
-
-	# create csv file of all products
-	with open("dbingredients.csv", 'w') as c:
-		# create the csv writer
-		writer = csv.writer(c)
-
-		c.write("Recipe, Ingredients\n")
-
-		#read from file of urls
-		with open(url_txt, 'r') as t:
-			for line in t.readlines(): 
-				myurl = line.strip()
-				print(myurl)
-				# write a row to the csv file
-				webpage_ingredients_to_row(writer, myurl)
-
-#; actually don't need anymore but maybe should keep as beautiful soup reference? 
-def webpage_ingredients_to_row(csvwriter, myurl): 
-	#https://www.geeksforgeeks.org/scrap-books-using-beautifulsoup-from-books-toscrape-in-python/
-	# variable to store website link as string
-	page_html = requests.get(myurl).text 
-	 
-	# call BeautifulSoup for parsing
-	page_soup = soup(page_html, "html.parser")
-
-	#recipe_name = page_soup.find("h1").getText()
-	#https://stackoverflow.com/questions/36651256/beautifulsoup-get-h2-text-without-class
-	h1_txt = page_soup.find("h1")
-	h1_txt_h2 = h1_txt.find(class_="h2")
-	recipe_name = h1_txt_h2.previous_sibling.strip()
-	print(recipe_name)
-
-	#recipe_ingredients = [x.get_text().strip() for x in page_soup.findAll("li", {"class": ['first ingredient', 'ingredient']})]
-	html_ingre = page_soup.findAll("li", {"class": ['first ingredient', 'ingredient']})
-
-	recipe_ingredients = []
-	for i in html_ingre:
-		#print(i.get_text().strip())
-		ingred = i.get_text().strip()
-		print('\t'+ingred)
-		recipe_ingredients.append(ingred)
-
-	# write a row to the csv file
-	csvwriter.writerow([recipe_name,recipe_ingredients])
-
-
+				csvwriter.writerow([recipe_title,recipe_page_num,recipe_ingredients])
 
 
 def read_ingredients_db(dbcsv): 
@@ -189,7 +114,7 @@ def main():
 	#df = df.applymap(lambda s: s.upper() if type(s) == str else s) #https://stackoverflow.com/questions/39512002/convert-whole-dataframe-from-lower-case-to-upper-case-with-pandas
 	print(df.head())
 
-	get_ingredient_recipes(df, ingredients)
+	#get_ingredient_recipes(df, ingredients)
 
 if __name__ == "__main__":
 	main()
